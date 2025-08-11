64adcec8de68369d2d04cc89aaf5c31cd5f5e29b
haqimiM9 <it.merchant9@outlook.com>
2025-08-11 17:58:45 +0800

    update: close domain

--- resources/views/livewire/rsvp/respond-user.blade.php
+++ resources/views/livewire/rsvp/respond-user.blade.php
@@ -1,5 +1,5 @@
 @section('title', ' · Répondez S\'il Vous Plaît')
-<div class="isolate md:isolate-auto flex flex-col gap-4">
+{{-- <div class="isolate md:isolate-auto flex flex-col gap-4">
     <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
         <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
             style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
@@ -42,5 +42,22 @@
     <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
         {{ __('Already responded? View the guest book!') }}
         <flux:link :href="route('guest_list')" wire:navigate>{{ __('here...') }}</flux:link>
+    </div>
+</div> --}}
+<div>
+    <div class="text-center">
+        <div class="mb-5">
+            <flux:separator variant="sublte" />
+        </div>
+        <flux:subheading class="text-center break-words">
+            {{ __('This domain is no longer accepting RSVPs. Please click the button below.') }}
+        </flux:subheading>
+        <div class="flex flex-row justify-center p-3">
+            <flux:button href="https://rsvp.laravel.cloud/event/nabilah-hakim" size="sm" variant="primary"
+                class="w-full">
+                {{ __('New RSVP') }}
+                <flux:icon icon="arrow-up-right" class="size-4" />
+            </flux:button>
+        </div>
     </div>
 </div>
